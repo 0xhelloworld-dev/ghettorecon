a05ab87a2e41e63210d64c8ef56c8b255e965a6f
0xghostwriter <jonathanchua17@gmail.com>
2020-10-12 23:53:42 +0000

    lotta changes

--- tools/ffufrecursive.sh
+++ tools/ffufrecursive.sh
@@ -6,7 +6,7 @@ echo "Beast Mode ON"
 
 targetfile=$3
 
-cd /root/Desktop/ghettobash/output/$targetfile/subdomainanalysis/ffuf
+cd /root/Desktop/ghettobash/output/$targetfile/subdomainanalysis/ffuf/401403
 #lists/content_discovery_nullenc0de.txt
 
 #ffuf -w "$httprobemasterlist:DOMAIN" -w $dirsearchwordlist -u DOMAIN/FUZZ -t 80 -recursion -recursion-depth 2 -maxtime-job 90 -o $ffuffolder/output.json
@@ -16,7 +16,7 @@ xargs -P10 -I {} sh -c 'url="{}"; ffuf -s -mc all -c -H "X-Forwarded-For: 127.0.
 
 cd /root/Desktop/ghettobash/
 
-cat /root/Desktop/ghettobash/output/$3/subdomainanalysis/ffuf/* | jq '[.results[]|{status: .status, length: .length, url: .url}]' | grep -oP "status\":\s(\d{3})|length\":\s(\d{1,7})|url\":\s\"(http[s]?:\/\/.*?)\"" | paste -d' ' - - - | awk '{print $2" "$4" "$6}' | sed 's/\"//g' > $2
+cat /root/Desktop/ghettobash/output/$3/subdomainanalysis/ffuf/401403/* | jq '[.results[]|{status: .status, length: .length, url: .url}]' | grep -oP "status\":\s(\d{3})|length\":\s(\d{1,7})|url\":\s\"(http[s]?:\/\/.*?)\"" | paste -d' ' - - - | awk '{print $2" "$4" "$6}' | sed 's/\"//g' > $2
 
 
 printf "\nDone. Result is stored in $2\n"
